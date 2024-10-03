719bbea34c956bf3dfb49b6dc3501e78d5d18c14
Miroslav Patrashkov <mpatrashkov2002@outlook.com>
2024-10-03 19:19:07 +0300

    fix

--- src/generate-png.ts
+++ src/generate-png.ts
@@ -53,7 +53,7 @@ let generatedFileContent = "";
 
 for (const countryPhoneCode of countriesPhoneCodes) {
   const iso = countryPhoneCode.iso.toLowerCase();
-  generatedFileContent += `export * as ${iso}Flag from "./flags/${iso}.png";\n`;
+  generatedFileContent += `export * as ${iso}Flag from "./flags/png/${iso}.png";\n`;
 }
 
 writeFileSync(flagsIndex, generatedFileContent);
